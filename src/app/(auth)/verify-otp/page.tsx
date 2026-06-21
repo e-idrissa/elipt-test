f94d6db8ac415ad1b59844528ea4cdc988a743f5
e-idrissa <id.hemedy98@gmail.com>
2026-06-21 09:12:32 +0200

    feat(ui): update verify-otp page

--- src/app/(auth)/verify-otp/page.tsx
+++ src/app/(auth)/verify-otp/page.tsx
@@ -1,6 +1,16 @@
+import { Logo } from "@/components/global/logo"
+import { VerifyOTPForm } from "@/features/auth/verify-otp-form"
+
 const VerifyOtpPage = () => {
   return (
-    <div>VerifyOtpPage</div>
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm space-y-6">
+        <div className="flex items-center justify-center">
+          <Logo />
+        </div>
+        <VerifyOTPForm />
+      </div>
+    </div>
   )
 }
 
---
+++ src/features/auth/verify-otp-form.tsx
@@ -0,0 +1,140 @@
+"use client";
+
+import * as z from "zod";
+import { Controller, useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Spinner } from "@/components/ui/spinner";
+
+const formSchema = z.object({
+  email: z.email(),
+  otp: z.string().min(4, "OTP is required."),
+});
+
+export const VerifyOTPForm = ({
+  className,
+  ...props
+}: React.ComponentProps<"div">) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      otp: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    toast("You submitted the following values:", {
+      description: (
+        <pre className="mt-2 w-[320px] overflow-x-auto rounded-md bg-code p-4 text-code-foreground">
+          <code>{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+      position: "bottom-right",
+      classNames: {
+        content: "flex flex-col gap-2",
+      },
+      style: {
+        "--border-radius": "calc(var(--radius)  + 4px)",
+      } as React.CSSProperties,
+    });
+  }
+
+  const {
+    handleSubmit,
+    control,
+    formState: { isValid, isSubmitting },
+  } = form;
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <form id="verification-form" onSubmit={handleSubmit(onSubmit)}>
+        <Card>
+          <CardHeader className="text-center">
+            <CardTitle className="text-xl">Verify your account</CardTitle>
+            <CardDescription>Enter the OTP code sent to your email.</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <FieldGroup>
+              <Controller
+                name="email"
+                control={control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid} className="hidden">
+                    <FieldLabel htmlFor="verification-form-email">Email</FieldLabel>
+                    <Input
+                      {...field}
+                      id="sign-in-form-email"
+                      aria-invalid={fieldState.invalid}
+                      type="email"
+                      placeholder="eddy@gmail.com"
+                      autoComplete="off"
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+              <Controller
+                name="otp"
+                control={control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <Input
+                      {...field}
+                      id="sign-in-form-password"
+                      aria-invalid={fieldState.invalid}
+                      type="password"
+                      placeholder="••••••••"
+                      autoComplete="off"
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+            </FieldGroup>
+          </CardContent>
+          <CardFooter>
+            <Field orientation="horizontal">
+              <Button
+                type="submit"
+                form="sign-in-form"
+                className="w-full"
+                disabled={isSubmitting || !isValid}
+              >
+                {isSubmitting ? (
+                  <>
+                    <Spinner />
+                    Verifying...
+                  </>
+                ) : (
+                  <>Verify</>
+                )}
+              </Button>
+            </Field>
+          </CardFooter>
+        </Card>
+      </form>
+    </div>
+  );
+};
